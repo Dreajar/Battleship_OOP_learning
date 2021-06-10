b6f70e3e56663b8b67ca2eeb507335a9ef14b84d
Luna <lmail888888@gmail.com>
2021-06-11 01:00:34 +0800

    Update Battleship_OOP_learning.py

--- Battleship_OOP_learning.py
+++ Battleship_OOP_learning.py
@@ -1,9 +1,8 @@
 import random
-
 ships = {'carrier': 5, 'battleship': 4, 'cruiser': 3, 'submarine': 3, 'destroyer': 2}
 moves = 10
 grid_length = 10
-board = []
+
 # ==== creating class BattleShip ==== #
 
 class BattleShip:
@@ -17,17 +16,6 @@ class BattleShip:
         self.x_coord = x_coord
         self.y_coord = y_coord
 
-    def provide_name(self):
-        return self.name
-
-    def hp_remaining(self):
-        return self.hp
-
-# ==== create board ==== #
-
-for x in range(grid_length):
-    board.append(["0"] * grid_length)
-
 # ==== print board ==== # !!!
 
 def print_board(board):
@@ -37,67 +25,53 @@ def print_board(board):
         
         if index != len(board)-1:
             print('-' * (len(board) * 2 - 1))
-        else:
-            None
 
 # ==== set board ==== #
 
-def set_ships(board):
-
+def set_ships():
     while True:
-    # generate random positions
-    # Note: can generate ship that will extend out of bounds
-    # Solution 1: catch error, try generating all ships again
-    # Solution 2: catch error, generate individual ship again
-
-    # might need a while loop here !!!
-
         try:
-
             board = [] # reset board - clears everything
 
             for x in range(grid_length):
                 board.append(["0"] * grid_length)
-
             # remakes 10x10 grid
 
             list_of_ships = []
-
             # clears list of ship objects
 
             ship_x = random.sample(range(10), 5) # generate x-value
             ship_y = random.sample(range(10), 5) # generate y-value
             
-
             for index, (name, length) in enumerate(ships.items()):
 
                 # generate random boolean for orientation
                 # True = to the right; False = downwards
                 orientation = random.choice([True, False])
-
-                list_of_ships.append(BattleShip(name, length, orientation))
-
                 x_coord = ship_x[index]
                 y_coord = ship_y[index]
 
+                list_of_ships.append(BattleShip(name, length, orientation))
                 list_of_ships[index].set_xy_coordinates(x_coord, y_coord)
                 
-                first_letter = [list_of_ships[index].name][0]
+                char1 = list_of_ships[index].name[0]
                 # first letter of name of ship type
 
                 if orientation == True:
-                    board[y_coord][x_coord:x_coord + length] = first_letter * length
+                    board[y_coord][x_coord:x_coord + length] = char1 * length
 
                 else:
                     for i in range(length):
-                        board[y_coord + i][x_coord] = first_letter
+                        board[y_coord + i][x_coord] = char1
 
         except IndexError:
             continue
 
         # redoes the entire thing again if outta index; if nothing fails, breaks outta loop
 
+        return board
+
         break
 
-set_ships(board)
+board = set_ships()
 print_board(board)
